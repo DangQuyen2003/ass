d50e80d8c10b1d30bdab72030b4380dbfca36c03
quangph22821 <quanghxph22821@fpt.edu.vn>
2023-05-31 23:28:31 +0700

    validate form add

--- src/app/admin/add-products/add-products.component.ts
+++ src/app/admin/add-products/add-products.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 import {Router} from '@angular/router'
 import { ProductService } from 'src/app/services/products.service';
 
@@ -9,15 +9,26 @@ import { ProductService } from 'src/app/services/products.service';
   styleUrls: ['./add-products.component.scss']
 })
 export class AddProductsComponent implements OnInit {
+  submit=false;
   constructor(private prodSrv: ProductService, private router: Router ) { }
   productFormCreate: FormGroup = new FormGroup({
-    name: new FormControl(),
-    price: new FormControl(),
-    image: new FormControl()
+    name: new FormControl("",Validators.required),
+    price: new FormControl("",Validators.required),
+    image: new FormControl("",Validators.required),
+    description: new FormControl("",[Validators.required, Validators.minLength(10)])
   });
   ngOnInit(): void {
   }
+  get sub (){
+    return this.productFormCreate.controls
+  }
   onCreate(){
+    this.submit = true;
+    console.log(this.productFormCreate.invalid);
+    
+    if(this.productFormCreate.invalid){
+      return;
+    }
     this.prodSrv.Create(this.productFormCreate.value).subscribe(data => {
       console.log(data);
       this.router.navigate(['admin'])
--- src/app/admin/edit-products/edit-products.component.ts
+++ src/app/admin/edit-products/edit-products.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router'
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Products } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/products.service';
@@ -12,12 +12,14 @@ import { ProductService } from 'src/app/services/products.service';
 })
 export class EditProductsComponent implements OnInit{
   id: string = '';
+  submit= false;
   products: Array<Products> = [];
   constructor(private prodSrv: ProductService, private route: ActivatedRoute, private router: Router) { }
   productFormEdit: FormGroup = new FormGroup({
-    name: new FormControl(),
-    price: new FormControl(),
-    image: new FormControl()
+    name: new FormControl("",Validators.required),
+    price: new FormControl("",Validators.required),
+    image: new FormControl("",Validators.required),
+    description: new FormControl("",[Validators.required, Validators.minLength(10)])
   });
 
   ngOnInit(): void {
@@ -28,17 +30,29 @@ export class EditProductsComponent implements OnInit{
     this.productFormEdit = new FormGroup({
       name: new FormControl(data.name),
       price: new FormControl(data.price),
-      image: new FormControl(data.image)
+      image: new FormControl(data.image),
+      description:new FormControl(data.description)
     });
     
    });
    
   }
 
+  get f(){
+    return this.productFormEdit.controls
+    
+    
+  }
+  
   onUpdate(){
-    this.prodSrv.Update(this.route.snapshot.params['id'], this.productFormEdit.value).subscribe(data => {
-      console.log(data);
-    })
-    this.router.navigate(['admin'])
+    this.submit = true;
+    console.log(this.productFormEdit.invalid);
+    if(this.productFormEdit.invalid){
+      return;
+    }
+    // this.prodSrv.Update(this.route.snapshot.params['id'], this.productFormEdit.value).subscribe(data => {
+    //   console.log(data);
+    // })
+    // this.router.navigate(['admin'])
   }
 }
